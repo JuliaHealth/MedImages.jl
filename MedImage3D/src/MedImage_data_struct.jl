a819fb6e6a2626328f40805d9adbd274d0c5d939
Jakub Mitura <jakub.mitura14@gmail.com>
2023-12-18 11:49:57 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

---
+++ MedImage3D/src/MedImage_data_struct.jl
@@ -0,0 +1,16 @@
+"""
+Here we define necessary data structures for the project.
+Main data structure is a MedImage object which is a 3D image with some metadata.
+
+!!!! Currently implemented as Struct but will be better to use as metadata arrays
+"""
+struct MedImage
+    pixel_array
+    direction
+    spacing
+    orientation
+    origin
+    date_of_saving::String
+    patient_id::String
+end
+
