96ad0bb0862e54a96bd098ad02d19a3531281814
Jakub Mitura <jakub.mitura14@gmail.com>
2024-01-06 19:25:25 +0000

    improved basic transformations tests
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/Basic_transformations.jl
+++ src/Basic_transformations.jl
@@ -19,16 +19,30 @@ end#rotate_mi
 
 
 """
-given a MedImage object and a Tuples that contains the the number of voxels we want to remove in each axs (x,y,z in order)
-from the begining of each axis (crop_beg) and from the end of each axis (crop_end)
-we are setting Interpolator by using Interpolator enum
+given a MedImage object and a Tuples that contains the location of the begining of the crop (crop_beg) and the size of the crop (crop_size) crops image
+we are setting Interpolator by using Interpolator enum (in basic implementation it will not be used)
 return the cropped MedImage object 
 """
-function crop_mi(im::Array{MedImage}, crop_beg::Tuple{Int64,Int64,Int64}, crop_end::Tuple{Int64,Int64,Int64}, Interpolator::Interpolator)::Array{MedImage}
+function crop_mi(im::Array{MedImage}, crop_beg::Tuple{Int64,Int64,Int64}, crop_size::Tuple{Int64,Int64,Int64}, Interpolator::Interpolator)::Array{MedImage}
 
   nothing
 
 end#crop_mi    
+
+
+"""
+given a MedImage object and a Tuples that contains the information on how many voxels to add in each axis (pad_beg) and on the end of the axis (pad_end)
+we are performing padding by adding voxels with value pad_val
+we are setting Interpolator by using Interpolator enum (in basic implementation it will not be used)
+return the cropped MedImage object 
+"""
+function pad_mi(im::Array{MedImage}, pad_beg::Tuple{Int64,Int64,Int64}, pad_end::Tuple{Int64,Int64,Int64},pad_val, Interpolator::Interpolator)::Array{MedImage}
+
+  nothing
+
+end#pad_mi    
+
+
 
 
 """
--- src/Resample_to_target.jl
+++ src/Resample_to_target.jl
@@ -2,6 +2,10 @@ include("./MedImage_data_struct.jl")
 """
 given two MedImage objects and a Interpolator enum value return the moving MedImage object resampled to the fixed MedImage object
 images should have the same orientation origin and spacing; their pixel arrays should have the same shape
+It require multiple steps some idea of implementation is below
+1) check origin of both images as for example in case origin of the moving image is not in the fixed image we need to return zeros
+2) we should define a grid on the basis of locations of the voxels in the fixed image and interpolate voxels from the moving image to the grid using GridInterpolations
+   in order to achieve it we need to use spatial metadata to get the correct locations of the voxels in the fixed and moving images
 """
 function resample_to_image(im_fixed::Array{MedImage}, im_moving::Array{MedImage}, Interpolator::Interpolator)::Array{MedImage}
 
