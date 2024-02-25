0b2d57d62cbf65fc58483456dcb05824e80f3df8
Jakub Mitura <jakub.mitura14@gmail.com>
2024-02-25 19:12:24 +0000

    completed dummy tests in basic transformations
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/Basic_transformations.jl
+++ src/Basic_transformations.jl
@@ -7,11 +7,11 @@ like translation, rotation, scaling, and cropping both input and output are MedI
 """
 given a MedImage object will rotate it by angle (angle) around axis (rotate_axis)
 the center of rotation is set to be center of the image
+It modifies both pixel array and not metadata
 we are setting Interpolator by using Interpolator enum
-mode will indicate do we want to modify just the pixel array, spatial metadata or both
 return the rotated MedImage object 
 """
-function rotate_mi(im::Array{MedImage}, rotate_axis::Int64,angle::Float64, Interpolator::Interpolator,mode::Mode_mi)::Array{MedImage}
+function rotate_mi(im::Array{MedImage}, rotate_axis::Int64,angle::Float64, Interpolator::Interpolator)::Array{MedImage}
 
   nothing
 
@@ -21,11 +21,11 @@ end#rotate_mi
 
 """
 given a MedImage object and a Tuples that contains the location of the begining of the crop (crop_beg) and the size of the crop (crop_size) crops image
+It modifies both pixel array and metadata
 we are setting Interpolator by using Interpolator enum (in basic implementation it will not be used)
-mode will indicate do we want to modify just the pixel array, spatial metadata or both
 return the cropped MedImage object 
 """
-function crop_mi(im::Array{MedImage}, crop_beg::Tuple{Int64,Int64,Int64}, crop_size::Tuple{Int64,Int64,Int64}, Interpolator::Interpolator,mode::Mode_mi)::Array{MedImage}
+function crop_mi(im::Array{MedImage}, crop_beg::Tuple{Int64,Int64,Int64}, crop_size::Tuple{Int64,Int64,Int64}, Interpolator::Interpolator)::Array{MedImage}
 
   nothing
 
@@ -35,11 +35,11 @@ end#crop_mi
 """
 given a MedImage object and a Tuples that contains the information on how many voxels to add in each axis (pad_beg) and on the end of the axis (pad_end)
 we are performing padding by adding voxels with value pad_val
+It modifies both pixel array and metadata
 we are setting Interpolator by using Interpolator enum (in basic implementation it will not be used)
-mode will indicate do we want to modify just the pixel array, spatial metadata or both
 return the cropped MedImage object 
 """
-function pad_mi(im::Array{MedImage}, pad_beg::Tuple{Int64,Int64,Int64}, pad_end::Tuple{Int64,Int64,Int64},pad_val, Interpolator::Interpolator,mode::Mode_mi)::Array{MedImage}
+function pad_mi(im::Array{MedImage}, pad_beg::Tuple{Int64,Int64,Int64}, pad_end::Tuple{Int64,Int64,Int64},pad_val, Interpolator::Interpolator)::Array{MedImage}
 
   nothing
 
@@ -50,11 +50,11 @@ end#pad_mi
 
 """
 given a MedImage object translation value (translate_by) and axis (translate_in_axis) in witch to translate the image return translated image
+It is diffrent from pad by the fact that it changes only the metadata of the image do not influence pixel array
 we are setting Interpolator by using Interpolator enum (in basic implementation it will not be used)
-mode will indicate do we want to modify just the pixel array, spatial metadata or both
 return the translated MedImage object
 """
-function translate_mi(im::Array{MedImage}, translate_by::Int64, translate_in_axis::Int64, Interpolator::Interpolator,mode::Mode_mi)::Array{MedImage}
+function translate_mi(im::Array{MedImage}, translate_by::Int64, translate_in_axis::Int64, Interpolator::Interpolator)::Array{MedImage}
 
   nothing
 
@@ -63,11 +63,11 @@ end#crop_mi
 
 """
 given a MedImage object and a Tuple that contains the scaling values for each axis (x,y,z in order)
+
 we are setting Interpolator by using Interpolator enum
-mode will indicate do we want to modify just the pixel array, spatial metadata or both
 return the scaled MedImage object 
 """
-function scale_mi(im::Array{MedImage}, scale::Tuple{Float64,Float64,Float64}, Interpolator::Interpolator,mode::Mode_mi)::Array{MedImage}
+function scale_mi(im::Array{MedImage}, scale::Tuple{Float64,Float64,Float64}, Interpolator::Interpolator)::Array{MedImage}
 
   nothing
 
