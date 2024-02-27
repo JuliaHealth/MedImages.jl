cb956f546e28c91ba6b7d96a8ad36ef6d1240a3e
Jakub Mitura <jakub.mitura14@gmail.com>
2024-02-27 07:32:16 +0000

    test decriptions
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/Resample_to_target.jl
+++ src/Resample_to_target.jl
@@ -4,7 +4,7 @@ given two MedImage objects and a Interpolator enum value return the moving MedIm
 images should have the same orientation origin and spacing; their pixel arrays should have the same shape
 It require multiple steps some idea of implementation is below
 1) check origin of both images as for example in case origin of the moving image is not in the fixed image we need to return zeros
-2) we should define a grid on the basis of locations of the voxels in the fixed image and interpolate voxels from the moving image to the grid using GridInterpolations
+2) we should define a grid on the basis of locations of the voxels in the fixed image and interpolate voxels from the moving image to the grid using for example GridInterpolations
    in order to achieve it we need to use spatial metadata to get the correct locations of the voxels in the fixed and moving images
 """
 function resample_to_image(im_fixed::Array{MedImage}, im_moving::Array{MedImage}, Interpolator::Interpolator)::Array{MedImage}
