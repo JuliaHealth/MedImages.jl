5e3140121adbabe2759fcc8d540131f31a48846f
hurtbadly <divita2004@gmail.com>
2024-04-20 09:29:27 +0530

    Completed nifit loading and saving

--- src/MedImage3D.jl
+++ src/MedImage3D.jl
@@ -1,5 +1,10 @@
 module MedImage3D
-
-greet() = print("Hello World!")
-
+include("./Nifti_image_struct.jl")
+include("./MedImage_data_struct.jl")
+include("./Load_and_save.jl")
+include("./Basic_transformations.jl")
+include("./Resample_to_target.jl")
+include("./Spatial_metadata_change.jl")
+include("./Resample_to_target.jl")
+include("./Utils.jl")
 end # module
