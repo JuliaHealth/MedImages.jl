3740fa51f1a77d39f90a7ac5bc2c1763b07e0595
Jakub Mitura <jakub.mitura@gmail.com>
2024-05-17 20:07:33 +0200

    improved change in spacing
    Signed-off-by: Jakub Mitura <jakub.mitura@gmail.com>

--- test/dicom_nifti.jl
+++ test/dicom_nifti.jl
@@ -76,12 +76,12 @@ function test_object_equality(medIm::MedImage,sitk_image)
     vox=permutedims(vox,(3,2,1))
     vv=vox-arr
     print("\n mmmmmmmmm $(maximum(vv)) $(maximum(arr))  \n")
+    print("vvvox arrr $(isapprox(arr ,vox; rtol =0.1)) \n ")
 
-    @test isapprox(arr ,vox; rtol =0.75)
+    @test isapprox(arr ,vox; rtol =0.1)
     # @test isapprox(arr[sx:end-sx,sy:end-sy,sz:end-sz]
     # ,medIm.voxel_data[sx:end-sx,sy:end-sy,sz:end-sz]; atol =0.1)
 
-    print("vvvox arrr $(isapprox(arr ,vox; rtol =0.1)) \n ")
     @test isapprox(collect(spacing), collect(Tuple{Float64,Float64,Float64}(medIm.spacing)); atol=0.1)
     # @test isapprox(spacing
     # ,medIm.spacing; atol =0.1)
