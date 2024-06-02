a2d5d7eec7d78a10e787880b8d314046204e1ffe
hurtbadly <divital2004@gmail.com>
2024-06-03 04:23:51 +0530

    Added module support for MedImage3D along with migration of documentation to documenterVitepress

---
+++ docs/build/.documenter/tutorials.md
@@ -0,0 +1,2 @@
+
+#Testing if it works
---
+++ docs/make.jl
@@ -0,0 +1,28 @@
+using MedImage3D
+using Documenter
+using DocumenterVitepress
+
+DocMeta.setdocmeta!(MedImage3D, :DocTestSetup, :(using MedImage3D); recursive=true)
+
+makedocs(;
+    modules = [MedImage3D],
+    repo = Remotes.GitHub("divital-coder", "MedImage.jl"),
+    authors = "Jakub-Mitura <jakubmitura14@gmail.com>, Divyansh-Goyal <divital2004@gmail.com> and contributors",
+    sitename = "MedImage3D.jl",
+    format = DocumenterVitepress.MarkdownVitepress(
+        repo="https://github.com/divital-coder/MedImage.jl",
+    ),
+    pages = [
+        "Home" => "index.md",
+        "Tutorials" => "tutorials.md",
+        "Contributing" => "contributing.md"
+    ],
+)
+
+deploydocs(;
+    repo = "github.com/divitla-coder/MedImage.jl",
+    target = "build", # this is where Vitepress stores its output
+    devbranch = "main",
+    branch = "gh-pages",
+    push_preview = true
+)
