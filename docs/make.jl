5eec8f83f5125939e28fd938f8180acf89e48833
divital-coder <divital2004@gmail.com>
2025-01-05 16:40:15 +0530

    Documentation setup

--- docs/make.jl
+++ docs/make.jl
@@ -4,23 +4,25 @@ using DocumenterVitepress
 
 DocMeta.setdocmeta!(MedImages, :DocTestSetup, :(using MedImages); recursive=true)
 
-makedocs(;
-    modules=[MedImages],
-    repo=Remotes.GitHub("JuliaHealth", "MedImage.jl"),
-    authors="Jakub-Mitura <jakubmitura14@gmail.com>, Divyansh-Goyal <divital2004@gmail.com> and contributors",
-    sitename="MedImages.jl",
-    format=DocumenterVitepress.MarkdownVitepress(
-        repo="https://github.com/JuliaHealth/MedImages.jl",
-    ),
-    pages=[
-        "Home" => "index.md",
+pgs = [ "Home" => "index.md",
         "Tutorials" => "tutorials.md",
         "Contributing" => "contributing.md"
-    ],
-)
+       ]
 
+fmt = DocumenterVitepress.MarkdownVitepress(
+      repo = "https://github.com/Juliahealth/MedImages.jl",)
+
+
+makedocs(;
+    modules=[MedImages],
+    repo=Remotes.GitHub("JuliaHealth", "MedImages.jl"),
+    authors="Jakub-Mitura <jakubmitura14@gmail.com>, Divyansh-Goyal <divital2004@gmail.com>, Jan-Zubik and contributors",
+    sitename="MedImages.jl",
+    format=fmt,
+    pages=pgs
+   )    
 deploydocs(;
-    repo="github.com/JuliaHealth/MedImage.jl",
+    repo="github.com/JuliaHealth/MedImages.jl",
     target="build", # this is where Vitepress stores its output
     devbranch="main",
     branch="gh-pages",
