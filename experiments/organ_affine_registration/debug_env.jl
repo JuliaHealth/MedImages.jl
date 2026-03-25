18f1acbf9daacca12d305bd9651dcb967c60cdd9
Jakub Mitura <jakub.mitura14@gmail.com>
2026-03-25 10:14:39 +0100

    up

--- experiments/organ_affine_registration/debug_env.jl
+++
@@ -1,8 +0,0 @@
-println("Julia environment check...")
-using MedImages
-println("MedImages loaded.")
-using CUDA
-println("CUDA functional: ", CUDA.functional())
-if CUDA.functional()
-    println("Device: ", CUDA.name(CUDA.device()))
-end
